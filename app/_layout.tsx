65598b92832fadeaaf352943bc2bf8498d186f24
Swarup Chanda <swarupchanda1963@gmail.com>
2025-03-09 23:54:21 +0530

    Updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import "./globals.css"
 import { Stack } from "expo-router";
+import "./globals.css";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      {/* <StatusBar hidden={true} /> */}
+
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
