844a8e81a5647142c41c242b59fa8777607f9032
Swarup Chanda <swarupchanda1963@gmail.com>
2025-03-10 00:49:07 +0530

    Updates

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Profile</Text>
     </View>
   )
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Search</Text>
     </View>
   )
 }
